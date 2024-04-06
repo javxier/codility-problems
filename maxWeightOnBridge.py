fb7ff4235b474173030710ad1c2705d22d2ff6da
Gerardo Orellana <jorellana2434@gmail.com>
2024-04-05 23:54:18 -0700

    comments edited

--- maxWeightOnBridge.py
+++ maxWeightOnBridge.py
@@ -12,28 +12,19 @@ def solution(U, weight):
     left, right = 0, num_cars - 1
     
     while left <= right:
-        if left == right:   # one car remaining
+        if left == right:                       # one car remaining
             count += 1
             break
 
-        if weight[left] + weight[right] <= U:  # if two cars that are heavy are allowed
+        if weight[left] + weight[right] <= U:   # if two cars that are heavy are allowed
             left += 1
             right -= 2
-        else:                       # heaviest car is not allowed to pass with others+
+        else:                                   # heaviest car is not allowed to pass with others+
             right -= 1
-        count += 1                  # cars turned back increment
+        count += 1                              # cars turned back increment
 
     return count
-
-    # maxWeight = U units at a time.
-    # N cars waiting to cross.
-    # weight = [] // weight of Kth car is weight[K] // first index is standard 0
-
-    # maxOnBridge = 2, following a queue order
-    # if one is removed, all following said car will move up one
-
     # find the minimum # of drivers turning back to prevent overload
 
     # an example // if we set the maxWeight to 9, we cannot have vehicles enter, they need to turn
     # N is an integer within the range [1... 1000000000]
-    pass
